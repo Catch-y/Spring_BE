60ce5c3533e120e42ae9a77fd6eace2412189bc6
alswjdghks <jerrymin1221@naver.com>
2025-01-06 21:11:56 +0900

    first commit

---
+++ src/main/java/umc/catchy/CatchyApplication.java
@@ -0,0 +1,13 @@
+package umc.catchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/catchy/CatchyApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.catchy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
