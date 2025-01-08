6664a9c64c303da09876f0d10a80e3ab5be61af8
StoneCAU <seokwoo1999@naver.com>
2025-01-09 04:17:10 +0900

    chore: 오타 수정

--- src/main/java/umc/catchy/domain/member/dto/request/LoginRequest.java
+++ src/main/java/umc/catchy/domain/member/dto/request/LoginRequest.java
@@ -12,7 +12,7 @@ public record LoginRequest(
                 regexp = RegexConstants.EMAIL_REGEX,
                 message = "유효한 이메일 주소여야 합니다."
         )
-        @Schema(example = "trippiece@example.com", description = "유효한 이메일 주소")
+        @Schema(example = "catch@example.com", description = "유효한 이메일 주소")
         String email,
 
         @NotNull(message = "유저 ID는 필수 입력 항목입니다.")
