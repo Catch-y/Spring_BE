d795eafb1515a19d2573444814e64eff1d05f5dd
StoneCAU <seokwoo1999@naver.com>
2025-01-09 03:08:35 +0900

    feat: 이메일 검증 정규식 상수 추가

---
+++ src/main/java/umc/catchy/global/common/constants/RegexConstants.java
@@ -0,0 +1,7 @@
+package umc.catchy.global.common.constants;
+
+public class RegexConstants {
+
+    // 정규식
+    public static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*\\.(com|net|org|co\\.kr|ac\\.kr|gov|edu)$";
+}
