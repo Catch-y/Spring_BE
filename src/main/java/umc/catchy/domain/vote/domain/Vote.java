26b3569bb21345796ac4a81e1b450914409c24f2
westjuuu <westjiuuu@naver.com>
2025-01-14 19:51:03 +0900

    feat: 투표 생성 API

--- src/main/java/umc/catchy/domain/vote/domain/Vote.java
+++ src/main/java/umc/catchy/domain/vote/domain/Vote.java
@@ -2,6 +2,7 @@ package umc.catchy.domain.vote.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import umc.catchy.domain.common.BaseTimeEntity;
 import umc.catchy.domain.group.domain.Groups;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Vote extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +25,12 @@ public class Vote extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Groups group;
+
+    public static Vote createVote(Groups group) {
+        Vote vote = new Vote();
+        vote.status = VoteStatus.IN_PROGRESS;
+        vote.endTime = LocalDateTime.now().plusDays(1);
+        vote.group = group;
+        return vote;
+    }
 }
