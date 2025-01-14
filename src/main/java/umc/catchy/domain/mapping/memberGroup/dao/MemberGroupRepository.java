8884985cbec0ca53cf8b5de443044b15e4e039cf
JIWOO <138380856+westjiuuu@users.noreply.github.com>
2025-01-14 19:09:28 +0900

    [feature] 캘린더-그룹 조회 (#36)
    * feat: 그룹 탈퇴
    
    * feat: 사용자가 속한 그룹 조회
    
    * fix: slice 기반 무한 스크롤

--- src/main/java/umc/catchy/domain/mapping/memberGroup/dao/MemberGroupRepository.java
+++ src/main/java/umc/catchy/domain/mapping/memberGroup/dao/MemberGroupRepository.java
@@ -1,13 +1,20 @@
 package umc.catchy.domain.mapping.memberGroup.dao;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import umc.catchy.domain.mapping.memberGroup.domain.MemberGroup;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MemberGroupRepository extends JpaRepository<MemberGroup, Long> {
     boolean existsByGroupIdAndMemberId(Long groupId, Long memberId);
     Optional<MemberGroup> findByGroupIdAndMemberId(Long groupId, Long memberId);
+    @Query("SELECT mg FROM MemberGroup mg WHERE mg.member.id = :memberId")
+    Slice<MemberGroup> findAllByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 }
