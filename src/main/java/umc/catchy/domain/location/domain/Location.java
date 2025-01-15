63ebf26cc1981545ee353d072019e58894f231e2
jerrymin1221 <jerrymin1221@naver.com>
2025-01-15 15:10:32 +0900

    [feature] 사용자 취향설문 5단계(선호 지역) (#39)
    * fix: .gitignore Local 환경 수정
    
    * feat: 사용자 선호지역 DAO,DTO 설계
    
    * feat: 사용자 선호지역 저장 서비스 구현
    
    * feat: 사용자 취향설문 5단계 API 설계 및 서비스 로직 수정
    
    * fix: 사용자 취향설문 5단계 서비스 다중 선택가능 조건으로 수정

--- src/main/java/umc/catchy/domain/location/domain/Location.java
+++ src/main/java/umc/catchy/domain/location/domain/Location.java
@@ -1,18 +1,27 @@
 package umc.catchy.domain.location.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import umc.catchy.domain.common.BaseTimeEntity;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Location extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "location_id")
     private Long id;
 
+    @NotNull(message = "원하는 지역의 시/도 를 선택하세요.")
     private String upperLocation;
-
+    @NotNull(message = "해당 지역의 내부 구를 선택하세요.")
     private String lowerLocation;
+
+    public static Location createLocation(String upperLocation, String lowerLocation) {
+        return Location.builder().upperLocation(upperLocation).lowerLocation(lowerLocation).build();
+    }
 }
