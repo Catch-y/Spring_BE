4e01dc8ec461c94bb0509d3be87f3849ac4b96ca
StoneCAU <seokwoo1999@naver.com>
2025-01-18 09:50:02 +0900

    fix: 코스 장소 수정 시 2개 이상 5개 이하가 되도록 수정

--- src/main/java/umc/catchy/domain/course/dto/request/CourseUpdateRequest.java
+++ src/main/java/umc/catchy/domain/course/dto/request/CourseUpdateRequest.java
@@ -1,5 +1,6 @@
 package umc.catchy.domain.course.dto.request;
 
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +10,11 @@ import org.springframework.web.multipart.MultipartFile;
 @Setter
 public class CourseUpdateRequest {
     private String courseName;
+
     private String courseDescription;
+
+    @Size(min = 2, max = 5, message = "장소는 2개 이상 5개 이하로 선택되어야 합니다.")
     private List<Long> placeIds;
+
     private MultipartFile profileImage;
 }
