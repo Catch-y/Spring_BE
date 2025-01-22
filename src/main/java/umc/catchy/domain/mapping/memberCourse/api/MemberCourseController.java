560a618b98e81f4a72ddb64969b3175e51ecfa2a
alswjdghks <jerrymin1221@naver.com>
2025-01-22 16:58:58 +0900

    fix: 북마크한 코스 무한 스크롤 응답 수정

--- src/main/java/umc/catchy/domain/mapping/memberCourse/api/MemberCourseController.java
+++ src/main/java/umc/catchy/domain/mapping/memberCourse/api/MemberCourseController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.catchy.domain.mapping.memberCourse.dto.response.MemberCourseResponse;
+import umc.catchy.domain.mapping.memberCourse.dto.response.MemberCourseSliceResponse;
 import umc.catchy.domain.mapping.memberCourse.service.MemberCourseService;
 import umc.catchy.global.common.response.BaseResponse;
 import umc.catchy.global.common.response.status.SuccessStatus;
@@ -19,9 +20,9 @@ public class MemberCourseController {
 
     @Operation(summary = "북마크된 코스 무한 스크롤 API", description = "북마크된 코스 정보들을 무한 스크롤로 보여줍니다.")
     @GetMapping("/mypage/bookmark")
-    public BaseResponse<Slice<MemberCourseResponse>> findAllCourseByBookmarked(@RequestParam int pageSize,
-                                                                                               @RequestParam(required = false) Long lastCourseId) {
-        Slice<MemberCourseResponse> response = memberCourseService.findAllCourseByBookmarked(pageSize, lastCourseId);
+    public BaseResponse<MemberCourseSliceResponse> findAllCourseByBookmarked(@RequestParam int pageSize,
+                                                                             @RequestParam(required = false) Long lastCourseId) {
+        MemberCourseSliceResponse response = memberCourseService.findAllCourseByBookmarked(pageSize, lastCourseId);
         return BaseResponse.onSuccess(SuccessStatus._OK,response);
     }
 }
