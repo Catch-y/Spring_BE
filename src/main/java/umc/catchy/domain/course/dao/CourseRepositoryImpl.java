83f813e235ff09a07dc69feb11f7a3bd0f82afec
hyoinkang <hyoin1704@gmail.com>
2025-01-29 23:05:14 +0900

    fix : 타입 변환으로 인한 MULT 에러 해결

--- src/main/java/umc/catchy/domain/course/dao/CourseRepositoryImpl.java
+++ src/main/java/umc/catchy/domain/course/dao/CourseRepositoryImpl.java
@@ -26,22 +26,18 @@ public class CourseRepositoryImpl implements CourseRepositoryCustom{
 
         //인기코스 가중치 계산 : (방문 수 * 0.3) + (평점 * 0.4) + (리뷰 수 * 0.2) + (찜하기 * 0.1)
         NumberExpression<Double> popularityScore =
-                course.participantsNumber.doubleValue().multiply(0.3)   //코스의 방문 수
-                .add(course.rating.doubleValue().multiply(0.4)) //코스의 평점
-                .add(Expressions.asNumber(  //코스의 리뷰 수 : query By courseReview
+                Expressions.numberTemplate(Double.class, "({0} * 0.3)", course.participantsNumber.coalesce(0L)) //코스의 방문 수
+                        .add(Expressions.numberTemplate(Double.class, "({0} * 0.4)", course.rating.coalesce(0.0)))  //코스의 평점
+                        .add(Expressions.numberTemplate(Double.class, "({0} * 0.2)",    //코스의 리뷰 개수
                                 JPAExpressions
-                                        .select(courseReview.count())
+                                        .select(courseReview.count().coalesce(0L))
                                         .from(courseReview)
-                                        .where(courseReview.course.id.eq(course.id))
-                                ).multiply(0.2)
-                        )
-                        .add(Expressions.asNumber(  //코스의 북마크 수 : query By memberCourse
+                                        .where(courseReview.course.id.eq(course.id))))
+                        .add(Expressions.numberTemplate(Double.class, "({0} * 0.1)",    //코스의 찜하기 개수
                                 JPAExpressions
-                                        .select(memberCourse.count())
+                                        .select(memberCourse.count().coalesce(0L))
                                         .from(memberCourse)
-                                        .where(memberCourse.course.id.eq(course.id))
-                                ).multiply(0.1)
-                        );
+                                        .where(memberCourse.course.id.eq(course.id).and(memberCourse.bookmark.eq(true)))));
 
         return queryFactory.selectFrom(course)
                 .orderBy(popularityScore.desc())
