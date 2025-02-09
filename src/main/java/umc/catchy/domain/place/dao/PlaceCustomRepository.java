4262bda1e14caa4f83675b4687e6a9c662878169
westjuuu <westjiuuu@naver.com>
2025-02-10 01:17:41 +0900

    fix: 무한스크롤

--- src/main/java/umc/catchy/domain/place/dao/PlaceCustomRepository.java
+++ src/main/java/umc/catchy/domain/place/dao/PlaceCustomRepository.java
@@ -2,6 +2,7 @@ package umc.catchy.domain.place.dao;
 
 import java.util.Map;
 import org.springframework.data.domain.Slice;
+import umc.catchy.domain.category.domain.BigCategory;
 import umc.catchy.domain.mapping.placeCourse.dto.response.PlaceInfoPreview;
 import umc.catchy.domain.place.domain.Place;
 
@@ -11,4 +12,5 @@ public interface PlaceCustomRepository {
     List<Place> findPlacesByDynamicFilters(List<Long> categoryIds, List<String> upperRegions, List<String> lowerRegions);
     List<Place> findRecommendedPlaces(List<Long> categoryIds, List<String> upperRegions, List<String> lowerRegions, Long memberId, int maxPlaces);
     Slice<PlaceInfoPreview> recommendPlacesByActivityData(Long memberId, Double latitude, Double longitude, List<Long> categoryIds, Map<Long, Integer> hourMap, int pageSize, int page);
+    Slice<Place> getPlacesByCategoryWithPaging(BigCategory bigCategory, String groupLocation, String alternativeLocation, int pageSize, Long lastPlaceId);
 }
