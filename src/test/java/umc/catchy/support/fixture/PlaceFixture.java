16e1f06e87ac15f68c6f2158f0f81885259f26ed
Seokwoo <seokwoo1999@naver.com>
2025-12-17 17:14:29 +0900

    fix(PlaceFixture): 테스트에서 NPE 발생하던 문제 해결
    - createPlace 시 추가 정보 입력

--- src/test/java/umc/catchy/support/fixture/PlaceFixture.java
+++ src/test/java/umc/catchy/support/fixture/PlaceFixture.java
@@ -1,14 +1,25 @@
 package umc.catchy.support.fixture;
 
+import umc.catchy.domain.category.domain.BigCategory;
+import umc.catchy.domain.category.domain.Category;
 import umc.catchy.domain.place.domain.Place;
 
 public class PlaceFixture {
 
     public static Place createPlace(Long id, double rating) {
+        Category category = Category.builder()
+                .id(1L)
+                .name("테스트 카테고리")
+                .bigCategory(BigCategory.CAFE)
+                .build();
+
         return Place.builder()
                 .id(id)
                 .placeName("테스트 장소 " + id)
                 .rating(rating)
+                .category(category)
+                .latitude(37.5665)
+                .longitude(126.9780)
                 .build();
     }
 }
